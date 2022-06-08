2e465d6a2d5097557eb47994868cc1f4db030b2f
Joyce <elmalehjoyce@gmail.com>
2022-06-08 22:52:18 +0200

    Ajout de la barre de recherche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FILM } from './film/mock-film-list';
+import { Film } from './film/film'
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  filmList: Film[] = FILM;
+  filmSelected: Film|undefined;
+
+  ngOnInit () {
+    console.table(this.filmList);
+  }
+
+  selectFilm(filmTitle: string) {
+    const film: Film|undefined = this.filmList.find(film => film.primaryTitle == filmTitle)
+    if(film) {
+      console.log('Vous avez sélectionné le film ' + film?.primaryTitle);
+      this.filmSelected = film;
+    } else {
+      console.log('Vous avez sélectionné un film qui n existe pas');
+      this.filmSelected = film;
+    }
+  }
 }
--- src/app/film/film.ts
+++ src/app/film/film.ts
@@ -1,11 +1,11 @@
 export class Film {
-    "tconst": string;
-    "titleType": string;
-    "primaryTitle": string;
-    "originalTitle": string;
-    "isAdult": string;
-    "startYear": string;
-    "endYear": string;
-    "runtimeMinutes": string;
-    "genres": Array<string>;  
+    tconst: string;
+    titleType: string;
+    primaryTitle: string;
+    originalTitle: string;
+    isAdult: string;
+    startYear: string;
+    endYear: string;
+    runtimeMinutes: string;
+    genres: Array<string>;  
 }
